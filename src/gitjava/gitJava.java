a8595fe330a9b8853c97799129785036ec9129d3
lr manojkumar <lrmanoj33@gmail.com>
2021-12-15 21:53:14 +0530

    base commit

---
+++ src/gitjava/gitJava.java
@@ -0,0 +1,17 @@
+package gitjava;
+
+public class gitJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int time=20;
+		if (time<18) { 
+		System.out.println("true");
+		}else {
+				System.out.println("false");
+		
+	
+
+	}
+
+}}
